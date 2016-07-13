bc561ff4b070d049c65749d4e9c2526fde40fd10
hirogwa <hirogwa@gmail.com>
2016-07-13 23:48:36 +0900

    define models

---
+++ olympia/models.py
@@ -0,0 +1,109 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+from olympia import app
+
+db = SQLAlchemy(app)
+
+
+class LogDay(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(8), primary_key=True)
+    resource_key = db.Column(db.String(1024), primary_key=True)
+    remote_ip = db.Column(db.String(15), primary_key=True)
+    user_agent = db.Column(db.String(65536), primary_key=True)
+    download_count = db.Column(db.Integer)
+
+    def __init__(self, user_id, date, resource_key, remote_ip, user_agent,
+                 download_count):
+        self.user_id = user_id
+        self.date = date
+        self.resource_key = resource_key
+        self.remote_ip = remote_ip
+        self.user_agent = user_agent
+        self.download_count = download_count
+
+
+class LogHour(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    hour = db.Column(db.String(10), primary_key=True)
+    resource_key = db.Column(db.String(1024), primary_key=True)
+    remote_ip = db.Column(db.String(15), primary_key=True)
+    user_agent = db.Column(db.String(65536), primary_key=True)
+    download_count = db.Column(db.Integer)
+
+    def __init__(self, user_id, hour, resource_key, remote_ip, user_agent,
+                 download_count):
+        self.user_id = user_id
+        self.hour = hour
+        self.resource_key = resource_key
+        self.remote_ip = remote_ip
+        self.user_agent = user_agent
+        self.download_count = download_count
+
+
+class LogEntryRaw(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    bucket_owner = db.Column(db.String(64))
+    bucket = db.Column(db.String(63))
+    time = db.Column(db.DateTime(timezone=True), index=True)
+    remote_ip = db.Column(db.String(15))
+    requester = db.Column(db.String(64))
+    request_id = db.Column(db.String(16))
+    operation = db.Column(db.String(50))
+    key = db.Column(db.String(1024))
+    request_uri = db.Column(db.String(1200))
+    http_status = db.Column(db.String(3))
+    error_code = db.Column(db.String(50))
+    bytes_sent = db.Column(db.Integer)
+    object_size = db.Column(db.Integer)
+    total_time = db.Column(db.Integer)
+    turnaround_time = db.Column(db.Integer)
+    referrer = db.Column(db.String(2083))
+    user_agent = db.Column(db.String(65536))
+    version_id = db.Column(db.String(100))
+
+    def __init__(self,
+                 bucket_owner,
+                 bucket,
+                 time,
+                 remote_ip,
+                 requester,
+                 request_id,
+                 operation,
+                 key,
+                 request_uri,
+                 http_status,
+                 error_code,
+                 bytes_sent,
+                 object_size,
+                 total_time,
+                 turnaround_time,
+                 referrer,
+                 user_agent,
+                 version_id):
+        self.bucket_owner = bucket_owner
+        self.bucket = bucket
+        self.time = _time(time)
+        self.remote_ip = remote_ip
+        self.requester = requester
+        self.request_id = request_id
+        self.operation = operation
+        self.key = key
+        self.request_uri = request_uri
+        self.http_status = http_status
+        self.error_code = error_code
+        self.bytes_sent = _int(bytes_sent)
+        self.object_size = _int(object_size)
+        self.total_time = _int(total_time)
+        self.turnaround_time = _int(turnaround_time)
+        self.referrer = referrer
+        self.user_agent = user_agent
+        self.version_id = version_id
+
+
+def _time(s):
+    return datetime.datetime.strptime(s, '[%d/%b/%Y:%H:%M:%S %z]')
+
+
+def _int(s):
+    return 0 if s == '-' else int(s)
